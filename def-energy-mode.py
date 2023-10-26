78f6c0d7082c877bbf6954a174e62e9bb43112a9
gustavochotti <106937773+gustavochotti@users.noreply.github.com>
2023-10-26 17:36:06 -0300

    Update def-energy-mode.py

--- def-energy-mode.py
+++ def-energy-mode.py
@@ -3,6 +3,8 @@
 # GUID do Esquema de Energia: 381b4222-f694-41f0-9685-ff5bb260df2e  (Equilibrado)
 # GUID do Esquema de Energia: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c  (Alto desempenho)
 
+# Para saber os códigos no seu sistema, digite "powercfg /l" no prompt de comando
+
 # Para alterar o modo de energia é utilizado o subprocess para realizar esse procedimento
 # O código a seguir exemplifica como é realizado este processo
 
